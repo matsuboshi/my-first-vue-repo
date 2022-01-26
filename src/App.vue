ad24218694b9682229c8637c4a69886575941f01
matsuboshi <matsuboshi@hotmail.com>
2022-01-25 22:37:01 -0300

    basic to do list on App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,106 @@
 <script>
+import api from './api'
 export default {
+  name: 'App',
   data() {
     return {
-      n1: "",
-      n2: "",
-    };
-  },
-  computed: {
-    total () {
-      return +this.n1 + +this.n2
+      list: [{ id: 1, title: 'hello world' }],
+      title: '',
+      isUpdateMode: false,
+      currentUpdateId: ''
     }
   },
   methods: {
-    handleClick () {
-      this.n1 = +this.n1 * 2
-      this.n2 = +this.n2 * 2
+    async handleDelete(id) {
+      await api(
+        '/tasks/' + id,
+        {
+          method: 'DELETE'
+        },
+        false
+      )
+      debugger
+      await this.revalidate()
+    },
+    async revalidate() {
+      const data = await api('/tasks')
+      this.list = data
+    },
+    async handleCreateSubmit() {
+      const data = { title: this.title, description: this.title }
+      await api('/tasks', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      })
+      this.title = ''
+      await this.revalidate()
+    },
+    async handleUpdateSubmit() {
+      const data = { title: this.title, description: this.title }
+      await api('/tasks/' + this.currentUpdateId, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      })
+      this.title = ''
+      await this.revalidate()
+      this.isUpdateMode = false
+      this.title = ''
+    },
+    async handleUpdate(id, title) {
+      this.currentUpdateId = id
+      this.isUpdateMode = true
+      this.title = title
     }
+  },
+  async created() {
+    await this.revalidate()
   }
-};
+}
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <input type="text" v-model="n1">
-  <input type="text" v-model="n2">
-  <span>{{total}}</span>
-  <button @click="handleClick">double</button>
+  <div>
+    <h1>Hello World</h1>
+    <form v-if="!isUpdateMode" @submit.prevent="handleCreateSubmit">
+      <input type="text" v-model="title" />
+      <button class="btn-create">CREATE</button>
+    </form>
 
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+    <form v-else @submit.prevent="handleUpdateSubmit">
+      <input type="text" v-model="title" />
+      <button class="btn-update">UPDATE</button>
+    </form>
+
+    <div>
+      <div class="card" v-for="item in list" :key="item.id">
+        <p>{{ item.title }}</p>
+        <button @click="handleUpdate(item.id, item.title)">EDIT</button>
+        <button @click="handleDelete(item.id)">DELETE</button>
+      </div>
+    </div>
+  </div>
 </template>
+
+<style scoped>
+div {
+  padding: 5px 5px 20px 5px;
+}
+
+.card {
+  border: solid 2px black;
+}
+
+.btn-create {
+  background-color: rgb(115, 255, 73);
+}
+
+.btn-update {
+  background-color: rgb(255, 255, 94);
+}
+</style>
