eee9fba0867e3670df55eb503270adf4a3183d5c
matsuboshi <matsuboshi@hotmail.com>
2022-01-25 22:13:51 -0300

    registering basic concepts taught by Eder

---
+++ src/BasicConcepts.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  data() {
+    return {
+      n1: '',
+      n2: ''
+    }
+  },
+  computed: {
+    total() {
+      return +this.n1 + +this.n2
+    }
+  },
+  methods: {
+    handleClick() {
+      this.n1 = +this.n1 * 2
+      this.n2 = +this.n2 * 2
+    }
+  }
+}
+</script>
+
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <input type="text" v-model="n1" />
+  <input type="text" v-model="n2" />
+  <span>{{ total }}</span>
+  <button @click="handleClick">double</button>
+
+  <HelloWorld msg="Hello Vue 3 + Vite" />
+</template>
